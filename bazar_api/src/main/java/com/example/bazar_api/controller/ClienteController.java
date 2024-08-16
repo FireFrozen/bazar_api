8b8d41f3d8bb13c31fa1169276ea9a53cd7e3251
FireFrozen <j.mendozaz@pucp.pe>
2024-08-16 12:08:06 -0500

    cors added

--- bazar_api/src/main/java/com/example/bazar_api/controller/ClienteController.java
+++ bazar_api/src/main/java/com/example/bazar_api/controller/ClienteController.java
@@ -5,20 +5,15 @@ import com.example.bazar_api.model.Cliente;
 import com.example.bazar_api.service.IClienteService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins ={"http://localhost","*"} )
 public class ClienteController {
     
     @Autowired
     IClienteService clienteServ;
-    
+
     @GetMapping("clientes")
     public List<Cliente> traerClientes(){
         
--- bazar_api/src/main/java/com/example/bazar_api/controller/ProductoController.java
+++ bazar_api/src/main/java/com/example/bazar_api/controller/ProductoController.java
@@ -5,14 +5,9 @@ import com.example.bazar_api.model.Producto;
 import com.example.bazar_api.service.IProductoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins ={"http://localhost","*"} )
 @RestController
 public class ProductoController {
     
